861f431a93ef0300b2aab509a8f009a4af4f65c9
ladcs <hpladcs@gmail.com>
2021-05-07 11:51:05 -0300

    requisito 7

--- script.js
+++ script.js
@@ -7,12 +7,23 @@ function addItenList() {
   let descItem = document.createElement('p');
   let lista = document.getElementById('lista-tarefas');
   descItem.innerHTML = desc;
+  descItem.className = 'itemSelecionado'
   lista.appendChild(item);
   item.appendChild(descItem);
   document.getElementById('texto-tarefa').value = '';
 }
 
+function paintBg(ent) {
+  ent.classList.add('selected');
+}
+
 window.onload = function main() {
-    let button = document.querySelector('#criar-tarefa');
-    button.addEventListener('click', addItenList);
+  let button = document.querySelector('#criar-tarefa');
+  button.addEventListener('click', addItenList);
+  document.addEventListener('click', function (event) {
+    let exporto = event.target;
+    if (exporto.className === 'itemSelecionado') {
+      paintBg(exporto);
+    }
+  })
 }
